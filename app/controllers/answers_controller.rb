af06847fbd65c70f3c890ffeebee347693a06f64
VictorKostiuk <vityok.kostiuk@gmail.com>
2022-02-08 00:25:25 +0200

    All green tests

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,6 +2,14 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params)
+    # respond_to do |format|
+    #   if @answer.save
+    #     # format.html { redirect_to @question, notice: 'Answer was successfully created.' }
+    #     format.js
+    #   else
+    #     format.js { render json: @answer.errors, status: :unprocessable_entity }
+    #     end
+    #   end
   end
 
   private
